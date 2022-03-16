b074947ec8dc3219ed13f03b8438136c5ff6c0a4
srith0798 <uzuhimawari98@gmail.com>
2022-03-16 11:01:45 +0530

    tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        blk: "#141414",
+        gry: "#F6FAFC",
+        bl: "#3D61B0",
+      },
+      screens: {
+        "min-lg": "985px",
+      },
+    },
+  },
+  plugins: [],
+};
